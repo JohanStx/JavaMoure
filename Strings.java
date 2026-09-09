ccc3b88a9253b13dbe5c99fa835550892ee318f4
JohanP <johan.zuluaga@campusucc.edu.co>
2026-09-09 15:05:36 -0500

    Strings

---
+++ Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        
+        //cadenas de texto
+
+        String name = "Johan";
+        var Surname = new String("Zuluaga");
+
+    }
+}
