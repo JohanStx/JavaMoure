9d9bb980ead41fc190c015e0194f05929b09dc86
JohanP <johan.zuluaga@campusucc.edu.co>
2026-09-15 15:37:54 -0500

    Trim, Replace, Format

--- Strings.java
+++ Strings.java
@@ -15,7 +15,7 @@ public class Strings {
         System.out.println(name.length());
 
         //obtener caracter
-        System.out.println(name.charAt(name.length() - 1));
+        System.out.println(name.charAt(name.length() - 1));//Da el ultimo caractar de la cadena
 
         //subcadena
 
@@ -24,8 +24,8 @@ public class Strings {
 
         //Mayusculas y minusculas
 
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());//Pasa la cadena a mayuscula
+        System.out.println(name.toLowerCase());//Pasa la cadena a minuscula
 
         //comprobar si contiene
         System.out.println("Hola, java".contains("Johan"));
@@ -36,10 +36,10 @@ public class Strings {
         //Comparacion
         System.out.println(name.equals("Johan"));
         System.out.println(name.equals("johan"));
-        System.out.println(name.equalsIgnoreCase("johan"));
+        System.out.println(name.equalsIgnoreCase("johan"));//ignora las masyuculas
         System.out.println();
 
-        // == va. equals
+        // == vs. equals
 
         var a = "Johan";
         var b = "Johan";
@@ -47,7 +47,24 @@ public class Strings {
 
         System.out.println(a == b);
         System.out.println(a == c);
-        System.out.println(a.equals(c));
+        System.out.println(a.equals(c));//Compara el valor como tal
+        System.out.println();
+
+        //Trim
+       
+        System.out.println(" Hola, me llamo Johan ".trim());// elimina espacios en blanco al principio y al final
+        System.out.println(" Hola, me llamo Johan ");
+
+        //Replece
+
+        System.out.println(" Hola, me llamo Johan ".replace(" ", ""));//quita los espacios reemplazando
+        System.out.println("Hola, me llamo Johan".replace("Johan ", "Stiven"));//reempleza el nombre johan, por el nombre Stiven
+        System.out.println();
+
+        //Format
+        var age = 17;
+        System.out.println(String.format("hola, %s. Tengo %d", name, age));
+
 
 
     }   
